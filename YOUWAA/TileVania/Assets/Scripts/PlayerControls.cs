8093f4cb4c70588534557929b0d4ab0d6091db60
Yousif Aldolaijan <aldolaij@usc.edu>
2019-04-14 22:49:27 +0300

    gravity manager file instead of player controls

--- YOUWAA/TileVania/Assets/Scripts/PlayerControls.cs
+++ YOUWAA/TileVania/Assets/Scripts/PlayerControls.cs
@@ -3,39 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class PlayerControls : MonoBehaviour {
 
     Rigidbody2D myRigidBody;
     BoxCollider2D myCollider;
 
-    float rotationGoal;
-    float defaultGravity;
+    public float rotationGoal;
     float totalDistance = 0f;
     bool didICollide;
     public float damageDone = 25f;
+    public bool directionChanged = false;
+    public Vector2 gravity;
+    GameObject game;
 
     [SerializeField] float health = 100;
-    [SerializeField] float gravityScale = 1f;
 
-    public Slider healthSlider;
+    //public Slider healthSlider;
 
     // Use this for initialization
     void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<BoxCollider2D>();
-        defaultGravity = -1 * Physics2D.gravity.y;
         didICollide = myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+        game = GameObject.Find("GameManager");
+        game.GetComponent<GravityManager>().AddEntity(this);
     }
 
     // Update is called once per frame
     void Update() {
-        GravityControl();
-        healthSlider.value = health;
-        if (health <= 0) {
-            Debug.Log("YOU DIE");
-            Destroy(gameObject);
-        }
+        SpriteRotation();
+        UpdateHealth();
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
@@ -48,50 +46,38 @@ public class PlayerControls : MonoBehaviour {
         }
     }
 
-    private void GravityControl() {
-        bool isCollidingWithGround = myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
-        bool direcitonChanged = false;
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            myRigidBody.gravityScale = gravityScale;
-            Physics2D.gravity = new Vector2(defaultGravity, 0f);
-            rotationGoal = 90;
-            direcitonChanged = true;
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            myRigidBody.gravityScale = gravityScale;
-            Physics2D.gravity = new Vector2(-defaultGravity, 0f);
-            rotationGoal = 270;
-            direcitonChanged = true;
-        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            myRigidBody.gravityScale = gravityScale;
-            Physics2D.gravity = new Vector2(0f, defaultGravity);
-            rotationGoal = 180;
-            direcitonChanged = true;
-        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            myRigidBody.gravityScale = gravityScale;
-            Physics2D.gravity = new Vector2(0f, -defaultGravity);
-            rotationGoal = 0;
-            direcitonChanged = true;
+    private void UpdateHealth() {
+        //if (healthSlider != null) {
+            //healthSlider.value = health;
+        //}
+        if (health <= 0) {
+            Debug.Log("YOU DIE");
+            Destroy(gameObject);
         }
+    }
+
+    private void SpriteRotation() {
+        directionChanged = false; // TODO: Update from GravityManager
+        bool isCollidingWithGround = myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
 
         // Raycast in the direction of gravity rather than velocity to avoid
         //  the need to wait for velocity to settle in the correct direction
         RaycastHit2D closestObsticle = Physics2D.Raycast(transform.position,
-            Physics2D.gravity.normalized,
+            gravity.normalized,
             10000f,
             LayerMask.GetMask("Ground"));
 
-        Debug.DrawRay(transform.position, Physics2D.gravity.normalized);
+        Debug.DrawRay(transform.position, gravity.normalized);
 
         float distanceToImpact = closestObsticle.distance;
 
-        if (direcitonChanged) {
+        if (directionChanged) {
             totalDistance = distanceToImpact;
         }
 
         // TODO: Rotating when you are in a corner
         // Somehow ensure that an object completely rotates before stopping
         float rotationFactor = isCollidingWithGround != didICollide && isCollidingWithGround ? 1 : 1 - (distanceToImpact / totalDistance);
-
 
         transform.rotation = Quaternion.Slerp(transform.rotation,
             Quaternion.Euler(0f, 0f, rotationGoal),
@@ -104,8 +90,6 @@ public class PlayerControls : MonoBehaviour {
         //}
 
         didICollide = isCollidingWithGround;
-
-
     }
 
 }
